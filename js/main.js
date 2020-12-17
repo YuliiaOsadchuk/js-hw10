3e57c9f878f0b3e7e79cc0d6b107f10f5c582f16
Julietty <julietty@Juliettys-MacBook-Pro.local>
2020-12-17 16:15:29 +0200

    added project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const letters = document.querySelectorAll(".letter-block");
+const audio = new Audio();
+letters.forEach((letter)=>letter.addEventListener('click', function(event){
+    audio.src = `./sound/${event.toElement.id}.mp3`;
+    audio.play();
+}))
+
+document.addEventListener('keydown', function(event){
+    audio.src = `./sound/${event.code}.mp3`;
+    audio.play();
+})
